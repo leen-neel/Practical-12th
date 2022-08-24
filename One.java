f3eb148b3a65a3e1723bb246dcdc8a18dba0af1c
leen-neel <and24903@gmail.com>
2022-08-24 11:15:29 +0530

    kill me and end my misery <3

---
+++ One.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class One {
+    
+    public static boolean isPrime(int n) {
+
+        int factors = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                factors++;
+            }
+        }
+
+        return factors == 2;
+
+    }
+
+    // This function reverses a number and checks if the number is equal to the
+    // parameter
+    public static boolean isPalindrome(int n) {
+
+        int revNum = 0;
+        int numberCopy = n;
+
+        int reminder = 0;
+
+        while (numberCopy > 0) {
+            reminder = numberCopy % 10;
+            revNum = (revNum * 10) + reminder;
+            numberCopy /= 10;
+        }
+
+        return revNum == n;
+
+    }
+
+    // This function takes the range as parameters and prints the numbers which are
+    // prime and palindrome
+    // The function prints the amount of values printed as well
+    public static void printValues(int m, int n) {
+
+        int count = 0;
+
+        for (int i = m; i <= n; i++) {
+            if (isPrime(i) && isPalindrome(i)) {
+                System.out.print(i + " ");
+                count++;
+            }
+        }
+
+        System.out.println(count == 0 ? "\nInvalid input" : "\nFrequency of prime palindrome numbers is : " + count);
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Enter the lower limit : ");
+        int m = Integer.parseInt(in.readLine());
+
+        System.out.print("Enter the upper limit : ");
+        int n = Integer.parseInt(in.readLine());
+
+        System.out.println();
+
+        if (m < 3000 && n < 3000) {
+            printValues(m, n);
+        } else {
+            System.out.println("Out of range.");
+        }
+
+    }
+
+}
