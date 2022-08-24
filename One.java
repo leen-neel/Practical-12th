23ae49f3f5c3cd702d27981a6dd85ba2ae592028
leen-neel <and24903@gmail.com>
2022-08-24 18:44:14 +0530

    fix

--- One.java
+++ One.java
@@ -8,6 +8,10 @@ public class One {
 
         int factors = 0;
 
+        if (n == 0 || n == 1) {
+            return false;
+        }
+
         for (int i = 1; i <= n; i++) {
             if (n % i == 0) {
                 factors++;
