fc1d8a2828f273994356d46f38be4e195ee7987b
leen-neel <and24903@gmail.com>
2022-10-27 09:32:59 +0530

    fixed class name

--- Sixteen.java
+++ Sixteen.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Fifteen {
+public class Sixteen {
 
     public static boolean isPalindrome(String str) {
 
