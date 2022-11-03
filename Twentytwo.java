02947beb9d295be55d67d348a3540d4b82dc1b5b
leen-neel <and24903@gmail.com>
2022-11-03 10:26:52 +0530

    22

--- Twentytwo.java
+++ Twentytwo.java
@@ -51,3 +51,15 @@ public class Twentytwo {
     }    
     
 }
+
+/*
+ * 
+ * Step 1: start
+ * Step 2: Take the number of teams as input and get their names in an array from the user
+ * Step 3: Get the word with the highest length and store the highest length in a variable
+ * Step 4: Now we loop through the 0 to highestLength (i) and we loop through the array inside the previous loop (j)
+ * Step 5: If j is greater than or equals to the length of the current team name, we print a tab space
+ * Step 6: Else we print the corresponding character
+ * Step 7: End 
+ * 
+ */
