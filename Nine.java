1ef5b8fe935ea2cbe6a01fd25634b4f3c5971da1
leen-neel <and24903@gmail.com>
2022-11-03 18:29:35 +0530

    fixes

--- Nine.java
+++ Nine.java
@@ -5,22 +5,22 @@ import java.io.InputStreamReader;
 public class Nine {
 
     static void sortTwoD(int arr[][]) {
-
-        for (int i = 0; i < arr.length; i++) {
- 
-            for (int j = 0; j < arr[i].length; j++) {
- 
-                for (int k = 0; k < arr[i].length - j - 1; k++) {
-                    if (arr[i][k] > arr[i][k + 1]) {
-                        int temp = arr[i][k];
-                        arr[i][k] = arr[i][k + 1];
-                        arr[i][k + 1] = temp;
-                    }
-                }
-            }
-        }
         
-        printArray(arr);
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+           for (int j = 0; j < n; j++) {
+              for (int k = 0; k < n; k++) {
+                 for (int l = 0; l < n; l++) {
+                    if (arr[i][j] < arr[k][l]) {
+                       // swap
+                       int temp = arr[i][j];
+                       arr[i][j] = arr[k][l];
+                       arr[k][l] = temp;
+                    }
+                 }
+              }
+           }
+        }
 
     }
 
@@ -36,10 +36,19 @@ public class Nine {
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-        int length = 3;
-        int breadth = 3;
+        System.out.print("M = ");
+        int length = Integer.parseInt(in.readLine());
+
+        System.out.print("N = ");
+        int breadth = Integer.parseInt(in.readLine());
+
+        System.out.println();
 
         int[][] arr = new int[length][breadth];
+
+        int largest = arr[0][0];
+        int largestRow = 0;
+        int largestCol = 0;
 
         for (int i = 0; i < arr.length; i++) {
 
@@ -48,17 +57,29 @@ public class Nine {
                 System.out.print("Enter data for position (" + i + ", " + j + ") : ");
                 arr[i][j] = Integer.parseInt(in.readLine());
 
+                if (arr[i][j] > largest) {
+                    largest = arr[i][j];
+                    largestRow = i;
+                    largestCol = j;
+                }
+
             }
 
         }
 
         System.out.println();
 
+        System.out.println("Original matrix : ");
         printArray(arr);
+        System.out.println("Largest number : " + largest);
+        System.out.println("Row : " + largestRow);
+        System.out.println("Column : " + largestCol);
 
         System.out.println();
 
+        System.out.println("Sorted matrix : ");
         sortTwoD(arr);
+        printArray(arr);
     }
 
 }
